44489647d785de3d24e71817902dff09255d497f
Yair Gemara <ygemara@gmail.com>
2024-07-24 17:22:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 import json
 creds_dict = st.secrets["gsheets"]
 # Define the scope
+
+if isinstance(creds_dict["private_key"], str):
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # Add credentials to the account
